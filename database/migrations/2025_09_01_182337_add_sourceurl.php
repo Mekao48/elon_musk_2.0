b52efb48e1aa5a6152900bb6fd4357850ca29121
Mekao48 <84495852+Mekao48@users.noreply.github.com>
2025-09-02 01:26:34 +0700

    update database 2

--- database/migrations/2025_09_01_182337_add_sourceurl.php
+++ database/migrations/2025_09_01_182337_add_sourceurl.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('news', function (Blueprint $table) {
-             $table->string('source_url')->nullable();
+            $table->string('source_url')->nullable();
             //
         });
     }
